85186cb4057ec3922de2148b67ae9064717cdcb1
Tim Kautz <tim-kautz@gmx.de>
2022-08-29 02:20:36 +0200

    Deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 DEBUG = True
 
 # <HINT> add your cloud host here
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlinecourse-4903-tika-accountable-llama-tv.eu-de.mybluemix.net']
+CSRF_TRUSTED_ORIGINS = ['https://onlinecourse-4903-tika-accountable-llama-tv.eu-de.mybluemix.net']
 
 
 # Application definition
